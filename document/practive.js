c08fd652691a06eacfbf0e864bb391cd54346e96
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-06-01 11:05:04 +0900

    演習4-2 終了

--- document/practive.js
+++ document/practive.js
@@ -6,9 +6,24 @@
 // console.log(lastName)
 // console.log(firstName)
 
+// let age = 10;
+// if (age < 20) {
+//     console.log("未成年です")
+// } else {
+//     console.log("成人です")
+// }
+
 let age = 10;
-if (age < 20) {
-    console.log("未成年です")
-} else {
-    console.log("成人です")
+if (age >= 0 && age <= 4) {
+    console.log("幼年期")
+} else if (age >= 5 && age <= 14) {
+    console.log("少年期")
+} else if (age >= 15 && age <= 24) {
+    console.log("青年期")
+} else if (age >= 25 && age <= 44) {
+    console.log("壮年期")
+} else if (age >= 45 && age <= 64) {
+    console.log("中年期")
+} else if (age >= 65) {
+    console.log("高年期")
 }
