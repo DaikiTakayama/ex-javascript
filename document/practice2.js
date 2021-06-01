63ba97d1fa52d8f5052090fd9dd429c95a37549c
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-06-01 12:16:02 +0900

    演習8

---
+++ document/practice2.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// function printName() {
+//     console.log('髙山大貴')
+// }
+
+// printName();
+
+function add(num1, num2) {
+    return (num1 + num2)
+}
+
+function sub(num1, num2) {
+    return (num1 - num2)
+}
+
+function multi(num1, num2) {
+    return (num1 * num2)
+}
+
+function div(num1, num2) {
+    return (num1 / num2)
+}
+
+console.log(add(5, 3))
