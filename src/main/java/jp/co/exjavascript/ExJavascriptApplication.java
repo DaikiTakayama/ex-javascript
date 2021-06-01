298866d24efbaeb5d55c17e5c37f782f3210a9e5
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-06-01 10:36:21 +0900

    first commit

---
+++ src/main/java/jp/co/exjavascript/ExJavascriptApplication.java
@@ -0,0 +1,13 @@
+package jp.co.exjavascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExJavascriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExJavascriptApplication.class, args);
+	}
+
+}
