3ac728e30aaa5250ef4003ee65e49b983b08b029
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-06-01 13:51:49 +0900

    演習11 まで終了

--- document/practice2.js
+++ document/practice2.js
@@ -6,13 +6,13 @@
 
 // printName();
 
-function add(num1, num2) {
-    return (num1 + num2)
-}
+// function add(num1, num2) {
+//     return (num1 + num2)
+// }
 
-function sub(num1, num2) {
-    return (num1 - num2)
-}
+// function sub(num1, num2) {
+//     return (num1 - num2)
+// }
 
 // function multi(num1, num2) {
 //     return (num1 * num2)
@@ -20,25 +20,48 @@ function sub(num1, num2) {
 
 
 
-function div(num1, num2) {
-    return (num1 / num2)
+// function div(num1, num2) {
+//     return (num1 / num2)
+// }
+// let printNameVariable = function() {
+//     console.log("だいき")
+// }
+
+// let addVariable = add(5, 3)
+
+// // console.log(add(5, 3))
+// console.log(addVariable)
+
+// let subVariable = sub(5, 3)
+// console.log(subVariable)
+
+// let multi = (num1, num2) => num1 * num2;
+// let multiVariable = multi(5, 3);
+// console.log(multiVariable)
+
+
+// let divVariable = div(5, 3);
+// console.log(divVariable)
+
+// let grobalVal = 10;
+
+// function func1() {
+//     let lovalVal = 20;
+//     console.log("globalVal:" + globalVal);
+//     console.log("localVal:" + locallVal);
+
+// }
+
+// func1();
+// console.log("globalVal:" + globalVal);
+// console.log("localVal:" + locallVal);
+
+function func1() {
+    x = 6;
+    if (true) {
+        var x = 5;
+    }
+    console.log(x);
 }
-let printNameVariable = function() {
-    console.log("だいき")
-}
 
-let addVariable = add(5, 3)
-
-// console.log(add(5, 3))
-console.log(addVariable)
-
-let subVariable = sub(5, 3)
-console.log(subVariable)
-
-let multi = (num1, num2) => num1 * num2;
-let multiVariable = multi(5, 3);
-console.log(multiVariable)
-
-
-let divVariable = div(5, 3);
-console.log(divVariable)
+func1();
