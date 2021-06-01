b603ff05fc9c9dcd2c08543f33a5849dfc6f005e
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-06-01 13:41:31 +0900

    演習9-2 終了

--- document/practice2.js
+++ document/practice2.js
@@ -14,9 +14,11 @@ function sub(num1, num2) {
     return (num1 - num2)
 }
 
-function multi(num1, num2) {
-    return (num1 * num2)
-}
+// function multi(num1, num2) {
+//     return (num1 * num2)
+// }
+
+
 
 function div(num1, num2) {
     return (num1 / num2)
@@ -25,6 +27,18 @@ let printNameVariable = function() {
     console.log("だいき")
 }
 
+let addVariable = add(5, 3)
+
+// console.log(add(5, 3))
+console.log(addVariable)
+
+let subVariable = sub(5, 3)
+console.log(subVariable)
+
+let multi = (num1, num2) => num1 * num2;
+let multiVariable = multi(5, 3);
+console.log(multiVariable)
 
 
-console.log(add(5, 3))
+let divVariable = div(5, 3);
+console.log(divVariable)
