7f81c07409ce4a9038dff101bc34d7e8d5c0e905
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-06-01 10:58:22 +0900

    演習4終了

--- document/practive.js
+++ document/practive.js
@@ -1,7 +1,14 @@
 // console.log("髙山大貴")
 
-let lastName = "髙山";
-let firstName = "大貴";
+// let lastName = "髙山";
+// let firstName = "大貴";
 
-console.log(lastName)
-console.log(firstName)
+// console.log(lastName)
+// console.log(firstName)
+
+let age = 10;
+if (age < 20) {
+    console.log("未成年です")
+} else {
+    console.log("成人です")
+}
